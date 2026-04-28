75868728102d56c24a4eefa30180015283b181bf
Damian <d.ceran729@gmail.com>
2026-04-28 16:33:20 +0200

    próba dodania rofi

--- home/default.nix
+++ home/default.nix
@@ -4,11 +4,15 @@
   # 1. Importujemy Twoje rozbite pliki konfiguracyjne z folderu home
   imports = [
     ./packages.nix
+    ./rofi.nix
     # Poniższe linie są zakomentowane. Odkomentuj je (usuń #),
     # gdy będziesz gotowy na wczytanie konfiguracji tych środowisk:
     # ./hyprland
     # ./niri
   ];
+
+
+
 
   home.username = "damian2120";
   home.homeDirectory = "/home/damian2120";
